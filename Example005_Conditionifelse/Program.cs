bc0212812915c87da38ff8fabf3b3e91769e5c3e
ArturGaliev <galiev_aa_94@mail.ru>
2022-08-07 00:50:07 +0300

    add Example005_Condition IFElse

---
+++ Example005_Conditionifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "артур")
+{
+    Console.WriteLine("Ура, это же Артур!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
